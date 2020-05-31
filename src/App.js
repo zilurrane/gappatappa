6041543bd1d0f1f5874a6672bd3ab85e44f26487
Zilu Ramkrishna Rane <zilurrane@gmail.com>
2020-05-31 12:00:44 +0530

    Clean and add UI code

---
+++ src/App.js
@@ -0,0 +1,58 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+} from 'react-native';
+
+import {
+  Colors
+} from 'react-native/Libraries/NewAppScreen';
+
+const App = () => {
+  return (
+    <>
+      <StatusBar barStyle="dark-content" />
+      <SafeAreaView>
+        <ScrollView
+          contentInsetAdjustmentBehavior="automatic"
+          style={styles.scrollView}>
+          <View style={styles.body}>
+            <Text style={styles.sectionTitle}>Gappa Tappa</Text>
+          </View>
+        </ScrollView>
+      </SafeAreaView>
+    </>
+  );
+};
+
+const styles = StyleSheet.create({
+  scrollView: {
+    backgroundColor: Colors.lighter,
+  },
+  body: {
+    backgroundColor: Colors.white,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    padding: 10,
+    fontWeight: '600',
+    color: Colors.black,
+    width: '100%',
+    textAlign: 'center',
+    backgroundColor: '#EEE'
+  }
+});
+
+export default App;
